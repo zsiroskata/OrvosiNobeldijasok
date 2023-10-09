d2b2e141e787b1bdbb7a26e54adf5c2087b5d366
Szent-Györgyi Technikum <szgyvs2020@gmail.com>
2023-10-09 09:29:18 +0200

    Add project files.

---
+++ OrvosiNobeldijasok/Nobeldijas.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrvosiNobeldijasok
+{
+    class Nobeldijas
+    {
+        //Év;Név;SzületésHalálozás;Országkód
+        public int Ev { get; set; }
+        public string Nev { get; set; }
+        public int SzuletesHalalozas { get; set; }
+        public string Orszagkod { get; set; }
+
+        public Nobeldijas(string sorok)
+        {
+            string[] sor = sorok.Split(";");
+            Ev = Convert.ToInt32(sor[0]);
+            Nev = sor[1];
+            //szuleteshalal 
+            Orszagkod = sor[4];
+        }
+    }
+}
---
+++ OrvosiNobeldijasok/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace OrvosiNobeldijasok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sr = new StreamReader(
+                path: @"..\..\..\orvosi_nobeldijak.txt",
+                encoding: System.Text.Encoding.UTF8
+                );
+            while (!sr.EndOfStream)
+            {
+                var sor = new Nobeldijas(sr.ReadLine());
+                nobeldijasok.Add(sor);
+            }
+        }
+
+        static List<Nobeldijas> nobeldijasok = new List<Nobeldijas>();
+    }
+}
